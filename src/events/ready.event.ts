16f4b7aa99876ea9bd87057d1a5f8c766af56d21
__Ciach0 <42491334+Ciach0@users.noreply.github.com>
2021-07-31 12:50:53 +0200

    Update ready.event.ts

--- src/events/ready.event.ts
+++ src/events/ready.event.ts
@@ -7,7 +7,7 @@ import { join } from 'path';
 export default class ReadyEvent implements IEvent {
     name: keyof ClientEvents = 'ready';
     async run(client: SlashClient): Promise<any> {
-        client.user.setActivity('AmpersandBot', {
+        client.user.setActivity('Slash Commands', {
             type: 'WATCHING'
         });
         for (const fileString of readdirSync(join(__dirname, '/../commands'))) {
@@ -18,6 +18,6 @@ export default class ReadyEvent implements IEvent {
         }
         await client.application.fetch();
         client.application?.commands.set(client.commands);
-        console.log(`${client.user.tag} is ready!`);
+        console.log(`${client.user.tag} is ready! Also, please wait for up to 30 minutes, because slash commands won't appear immediately after bot start!`);
     }
 }
